3a97848d589c797049fd6528d958ea756969102b
Hugo Perdomo <hugo.j.dev@gmail.com>
2020-09-12 16:25:25 -0400

    Initial commit

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import "../styles/global.css";
+
+/**
+ * Mainly used for importing globals
+ *
+ */
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
